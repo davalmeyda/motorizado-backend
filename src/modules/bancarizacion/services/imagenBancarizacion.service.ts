898bc49cda44252921055d6d1ff9f3284d5e8ea2
Peter <jnr202017@gmail.com>
2024-07-24 09:02:28 -0500

    cambios agregados

--- src/modules/bancarizacion/services/imagenBancarizacion.service.ts
+++ src/modules/bancarizacion/services/imagenBancarizacion.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { ImagenBancarizacion } from '../entities/imagen_bancarizacion.entity';
 import { identity } from 'rxjs';
 
@@ -11,30 +11,40 @@ export class ImagenBancarizacionService {
 		private readonly imagenBancarizacionRepository: Repository<ImagenBancarizacion>,
 	) {}
 
-	async create(pedido_id: number,user_id: number, name: string, url: string, cant_vouchers: number , pdf: any) {
-
+	async create(
+		pedido_id: number,
+		user_id: number,
+		name: string,
+		url: string,
+		cant_vouchers: number,
+		pdf: any,
+	) {
 		const lastIdEnvio = await this.imagenBancarizacionRepository.query(
 			`SELECT MAX(id) as id FROM imagen_bancarizacion`,
 		);
 
-		
 		const imagen: ImagenBancarizacion = new ImagenBancarizacion();
-		imagen.id = parseInt(lastIdEnvio[0].id ?? 0) + 1
+		imagen.id = parseInt(lastIdEnvio[0].id ?? 0) + 1;
 		imagen.pedido_id = pedido_id;
 		imagen.user_id = user_id;
-        imagen.name = name;
-        imagen.url = url;
-        imagen.cant_vouchers = cant_vouchers;
-        imagen.pdf_size = pdf;
+		imagen.name = name;
+		imagen.url = url;
+		imagen.cant_vouchers = cant_vouchers;
+		imagen.pdf_size = pdf;
 		imagen.created_at = new Date();
 		imagen.updated_at = new Date();
 		return await this.imagenBancarizacionRepository.save(imagen);
 	}
 
 	async findAll(UserId: number) {
+		const startOfDay = new Date();
+		startOfDay.setHours(0, 0, 0, 0);
+		const endOfDay = new Date();
+		endOfDay.setHours(23, 59, 59, 999);
 		return await this.imagenBancarizacionRepository.find({
 			where: {
 				user_id: UserId,
+				created_at: Between(startOfDay, endOfDay),
 			},
 		});
 	}
